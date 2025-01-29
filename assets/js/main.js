417fae81ecf88ff566d4bb253fd9b0834f444cbf
Triet <trietmt9@gmail.com>
2025-01-29 22:40:18 +0700

    setting up Js

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const navMenu = document.getElementById("nav-menu"), 
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close"),
+// =================== SHOW MENU ===================
+
+
+// =================== MENU SHOW ===================
+
+
+var swiper = new Swiper(".testimonial-wrapper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination:
+    {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
